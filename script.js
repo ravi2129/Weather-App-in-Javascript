f21a046a03a0fa8e4bbfd443332888398dd1e043
ravi2129 <ravirajdange1234@gmail.com>
2021-08-10 16:57:06 +0530

    Commit

---
+++ script.js
@@ -0,0 +1,41 @@
+let weather ={
+apikey:"4a19db00501d2159dada6de2b783b92f",
+ fetchWeather: function(city){
+     fetch(
+         "https://api.openweathermap.org/data/2.5/weather?q="
+         + city 
+         +"&units=metric&appid=4a19db00501d2159dada6de2b783b92f"
+         )
+     .then((response) => response.json())
+     .then((data) => this.displayWeather(data));
+ },
+ displayWeather: function(data){
+     console.log(data);
+     const { name }=data;
+     const { icon,description }=data.weather[0];
+     const { temp, humidity }=data.main;
+     const { speed }=data.wind;
+     console.log(name,icon,description,temp,humidity,speed);
+     document.querySelector(".city").innerText ="Weather in "+ name;
+     document.querySelector(".icon").src= "https://openweathermap.org/img/wn/"+icon+".png"; 
+     document.querySelector(".description").innerText=description;
+     document.querySelector(".temp").innerText=temp+"°C";
+     document.querySelector(".humidity").innerText="Humidity :" + humidity +"%";
+     document.querySelector(".wind").innerText="Wind Speed" + speed +"km/h";
+     document.querySelector(".weather").classList.remove("loading");
+     document.body.style.backgroundImage="url('https://source.unsplash.com/1600x900/?"+ name +"')"
+    },
+    search: function(){
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+};
+document.querySelector(".search button").addEventListener("click",function(){
+weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup",function(event){
+    if (event.key=="Enter"){
+        weather.search();
+    }
+});
+weather.fetchWeather("Delhi");
